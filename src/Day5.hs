9a1fd2c8af4d23f2fa0d5e059f183464a8ea198f
jang <jang@ioctl.org>
2021-01-02 16:02:52 +0000

    Day 5 part b

--- src/Day5.hs
+++ src/Day5.hs
@@ -64,6 +64,20 @@ day5 ls =
   in  run 0 prog 0
 
 {-
+--- Part Two ---
+
+Now, the jumps are even stranger: after each jump, if the offset was three or more, instead decrease it by 1. Otherwise, increase it by 1 as before.
+
+Using this rule with the above example, the process now takes 10 steps, and the offset values after finding the exit are left as 2 3 2 3 -1.
+
+How many steps does it now take to reach the exit?
 -}
 
-day5b ls = "hello world"
+run' n prog pc =
+  case Map.lookup pc prog of
+    Nothing -> n
+    Just x -> run' (succ n) (Map.insert pc (if x >= 3 then x - 1 else x + 1) prog) (pc + x)
+
+day5b ls =
+  let prog = parse ls
+  in  run' 0 prog 0
